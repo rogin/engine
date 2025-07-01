nextgenhealthcare/connect

commit 70eba57551192a8ee62d604a959f06fb81f38fc6

MIRTH-3024 The MirthTreeTable will now persist the order of columns in addition to saving its sorted order.
The table has a mouseListener registered on the tableHeader which is responsible for saving the sorted order of columns. Except for any metaDataColumns (applicable only to the MessageBrowser), the column order is saved in a map, serialized as XML and stored within a preference node. If a new column is added to the panel, the column ordering will be reset to its default order, otherwise column ordering will be restored from this map.

The MessageBrowser has been updated to now persist the order of columns. MetaDataColumn preferences will only persist across the same session. Logging out and back in will reset these columns to their default states.

The DashBoard panel has also been updated to persist column ordering since the underlying component is the same MirthTreeTable.
Issue: MIRTH-3024

client/src/com/mirth/connect/client/ui/browsers/message/MessageBrowser.java
@@ -35,6 +35,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -71,7 +72,6 @@ import org.jdesktop.swingx.decorator.Highlighter;
 import org.jdesktop.swingx.decorator.HighlighterFactory;
 import org.jdesktop.swingx.table.ColumnControlButton;
 import org.jdesktop.swingx.table.TableColumnExt;
-import org.jdesktop.swingx.table.TableColumnModelExt;
 import org.syntax.jedit.SyntaxDocument;
 import org.syntax.jedit.tokenmarker.XMLTokenMarker;
 
@@ -151,6 +151,7 @@ public class MessageBrowser extends javax.swing.JPanel {
     private String channelId;
     private Map<Integer, String> connectors;
     private List<MetaDataColumn> metaDataColumns;
+    private List<String> metaDataColumnNames;
     private MessageBrowserTableModel tableModel;
     private PaginatedMessageList messages;
     private Map<Long, Message> messageCache;
@@ -304,7 +305,7 @@ public class MessageBrowser extends javax.swing.JPanel {
         columnList.addAll(columnMap.values());
 
         // Add custom columns
-        List<String> metaDataColumnNames = new ArrayList<String>();
+        metaDataColumnNames = new ArrayList<String>();
 
         for (MetaDataColumn column : metaDataColumns) {
             metaDataColumnNames.add(column.getName());
@@ -313,6 +314,7 @@ public class MessageBrowser extends javax.swing.JPanel {
         Set<String> hiddenCustomColumns = customHiddenColumnMap.get(channelId);
         if (hiddenCustomColumns == null) {
             hiddenCustomColumns = new HashSet<String>();
+            hiddenCustomColumns.addAll(metaDataColumnNames);
             customHiddenColumnMap.put(channelId, hiddenCustomColumns);
         } else {
             // If this channel was viewed before, remove any hidden custom columns that no longer exist
@@ -331,24 +333,20 @@ public class MessageBrowser extends javax.swing.JPanel {
         MessageBrowserTableColumnFactory columnFactory = (MessageBrowserTableColumnFactory) messageTreeTable.getColumnFactory();
         for (int modelIndex = 0; modelIndex < columnList.size(); modelIndex++) {
             TableColumnExt column = columnFactory.createAndConfigureTableColumn(messageTreeTable.getModel(), modelIndex);
-            String columnName = column.getTitle();
-
-            boolean defaultVisible = false;
-            if (defaultVisibleColumns.contains(columnName)) {
-                defaultVisible = true;
-            }
-
-            // For system columns, check the preferences to see if they should be visible.
-            // Custom metadata columns will always be visible for now.
-            if (modelIndex < columnMap.size()) {
-                column.setVisible(Preferences.userNodeForPackage(Mirth.class).getBoolean("messageBrowserVisibleColumn" + columnName, defaultVisible));
-            } else {
-                columnFactory.configureCustomColumn(column, metaDataColumns.get(modelIndex - columnMap.size()).getType());
-
-                column.setVisible(!hiddenCustomColumns.contains(columnName));
-            }
-
             messageTreeTable.addColumn(column);
+        }
+
+        Set<String> channelSpecificColumns = new LinkedHashSet<String>();
+        channelSpecificColumns.addAll(metaDataColumnNames);
+        messageTreeTable.setMetaDataColumns(channelSpecificColumns);
+        messageTreeTable.setDefaultVisibleColumns(defaultVisibleColumns);
+        messageTreeTable.restoreColumnOrder(tableModel);
+
+        // Enable/Disable cached metaDataColumns
+        if (customHiddenColumnMap.size() > 0) {
+            for (String col : customHiddenColumnMap.get(channelId)) {
+                messageTreeTable.getColumnExt(col).setVisible(true);
+            }
         }
 
         runSearch();
@@ -1012,7 +1010,7 @@ public class MessageBrowser extends javax.swing.JPanel {
         columnMap.put(IMPORT_CHANNEL_ID_COLUMN, "Import Channel Id");
         columnMap.put(ORIGINAL_SERVER_ID_COLUMN, "Original Server Id");
 
-        defaultVisibleColumns = new HashSet<String>();
+        defaultVisibleColumns = new LinkedHashSet<String>();
         defaultVisibleColumns.add(columnMap.get(ID_COLUMN));
         defaultVisibleColumns.add(columnMap.get(CONNECTOR_COLUMN));
         defaultVisibleColumns.add(columnMap.get(STATUS_COLUMN));
@@ -1231,18 +1229,15 @@ public class MessageBrowser extends javax.swing.JPanel {
                     if (enable || messageTreeTable.getColumnCount() > 1) {
                         column.setVisible(enable);
 
-                        if (columnMap.values().contains(columnName)) {
-                            Preferences.userNodeForPackage(Mirth.class).putBoolean("messageBrowserVisibleColumn" + columnName, enable);
-                        } else {
-                            Set<String> customHiddenColumns = customHiddenColumnMap.get(channelId);
+                        Set<String> customHiddenColumns = customHiddenColumnMap.get(channelId);
 
-                            if (enable) {
-                                customHiddenColumns.remove(columnName);
-                            } else {
-                                customHiddenColumns.add(columnName);
-                            }
+                        if (enable) {
+                            customHiddenColumns.add(columnName);
+                        } else {
+                            customHiddenColumns.remove(columnName);
                         }
                     }
+                    messageTreeTable.saveColumnOrder();
                 }
             });
 
@@ -1280,23 +1275,8 @@ public class MessageBrowser extends javax.swing.JPanel {
 
             @Override
             public void actionPerformed(ActionEvent arg0) {
-
-                for (TableColumn column : ((TableColumnModelExt) messageTreeTable.getColumnModel()).getColumns(true)) {
-                    TableColumnExt columnExt = (TableColumnExt) column;
-                    String columnName = columnExt.getTitle();
-
-                    if (columnExt.getModelIndex() < columnMap.size()) {
-                        boolean enable = defaultVisibleColumns.contains(columnName);
-
-                        columnExt.setVisible(enable);
-                        Preferences.userNodeForPackage(Mirth.class).putBoolean("messageBrowserVisibleColumn" + columnName, enable);
-                    } else {
-                        columnExt.setVisible(true);
-
-                        Set<String> customHiddenColumns = customHiddenColumnMap.get(channelId);
-                        customHiddenColumns.remove(columnName);
-                    }
-                }
+                defaultVisibleColumns.addAll(metaDataColumnNames);
+                messageTreeTable.restoreDefaults(defaultVisibleColumns);
             }
 
         });
@@ -2216,7 +2196,7 @@ public class MessageBrowser extends javax.swing.JPanel {
         attachmentsPane = new javax.swing.JScrollPane();
         attachmentTable = null;
         messageScrollPane = new javax.swing.JScrollPane();
-        messageTreeTable = new com.mirth.connect.client.ui.components.MirthTreeTable();
+        messageTreeTable = new com.mirth.connect.client.ui.components.MirthTreeTable("messageBrowser", defaultVisibleColumns);
         jPanel1 = new javax.swing.JPanel();
         pageNumberLabel = new javax.swing.JLabel();
         mirthDatePicker1 = new com.mirth.connect.client.ui.components.MirthDatePicker();

client/src/com/mirth/connect/client/ui/DashboardPanel.java
@@ -21,6 +21,7 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -83,6 +84,7 @@ public class DashboardPanel extends javax.swing.JPanel {
     private boolean showLifetimeStats = false;
 
     private JMenuItem menuItem;
+    private Set<String> defaultVisibleColumns;
 
     public DashboardPanel() {
         this.parent = PlatformUI.MIRTH_FRAME;
@@ -105,6 +107,9 @@ public class DashboardPanel extends javax.swing.JPanel {
             }
         };
         tabs.addChangeListener(changeListener);
+
+        defaultVisibleColumns = new LinkedHashSet<String>();
+        defaultVisibleColumns.addAll(Arrays.asList(defaultColumns));
 
         makeStatusTable();
         loadTablePlugins();
@@ -210,8 +215,10 @@ public class DashboardPanel extends javax.swing.JPanel {
             }
         }
 
+        defaultVisibleColumns.addAll(columns);
+
         if (statusTable == null) {
-            statusTable = new MirthTreeTable("dashboardPanel");
+            statusTable = new MirthTreeTable("dashboardPanel", defaultVisibleColumns);
         }
 
         statusTable.setColumnFactory(new DashboardTableColumnFactory());
@@ -230,6 +237,7 @@ public class DashboardPanel extends javax.swing.JPanel {
         statusTable.putClientProperty("JTree.lineStyle", "Horizontal");
         statusTable.setAutoCreateColumnsFromModel(false);
         statusTable.setShowGrid(true, true);
+        statusTable.restoreColumnOrder(model);
 
         // TODO: try using a custom tree cell renderer to set custom icons on connectors, so that we can display a chain icon to indicate that a destination waits for the previous one
         // http://www.java.net/forum/topic/javadesktop/java-desktop-technologies/swinglabs/jxtreetable-custom-icons-node
@@ -335,7 +343,7 @@ public class DashboardPanel extends javax.swing.JPanel {
         menuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                statusTable.restoreDefaults();
+                statusTable.restoreDefaults(defaultVisibleColumns);
             }
         });
 

client/src/com/mirth/connect/client/ui/browsers/message/MessageBrowser.form
@@ -794,6 +794,9 @@
           <Layout class="org.netbeans.modules.form.compat2.layouts.support.JScrollPaneSupportLayout"/>
           <SubComponents>
             <Component class="com.mirth.connect.client.ui.components.MirthTreeTable" name="messageTreeTable">
+              <AuxValues>
+                <AuxValue name="JavaCodeGenerator_CreateCodeCustom" type="java.lang.String" value="new com.mirth.connect.client.ui.components.MirthTreeTable(&quot;messageBrowser&quot;, defaultVisibleColumns)"/>
+              </AuxValues>
             </Component>
           </SubComponents>
         </Container>

client/src/com/mirth/connect/client/ui/components/MirthTreeTable.java
@@ -14,8 +14,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.prefs.Preferences;
 
 import javax.swing.AbstractAction;
@@ -23,8 +28,13 @@ import javax.swing.Action;
 import javax.swing.JComponent;
 import javax.swing.KeyStroke;
 import javax.swing.SortOrder;
+import javax.swing.table.TableColumn;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.jdesktop.swingx.table.TableColumnExt;
+import org.jdesktop.swingx.table.TableColumnModelExt;
+import org.jdesktop.swingx.treetable.TreeTableModel;
 
 import com.mirth.connect.client.ui.AbstractDashboardTableNode;
 import com.mirth.connect.client.ui.Mirth;
@@ -38,11 +48,14 @@ public class MirthTreeTable extends SortableTreeTable {
     private Preferences userPreferences;
     private String prefix;
 
+    private Set<String> defaultVisibleColumns;
+    private Set<String> metaDataColumns;
+
     public MirthTreeTable() {
-        this(null);
+        this(null, null);
     }
 
-    public MirthTreeTable(String prefix) {
+    public MirthTreeTable(String prefix, Set<String> defaultVisibleColumns) {
         this.setDragEnabled(true);
         this.addKeyListener(new KeyListener() {
 
@@ -64,9 +77,70 @@ public class MirthTreeTable extends SortableTreeTable {
          * override it.
          */
         this.putClientProperty("terminateEditOnFocusLost", Boolean.FALSE);
+        this.getTableHeader().addMouseListener(new MouseAdapter() {
+            public void mouseReleased(MouseEvent e) {
+                saveColumnOrder();
+            }
+        });
 
         userPreferences = Preferences.userNodeForPackage(Mirth.class);
         this.prefix = prefix;
+        this.defaultVisibleColumns = defaultVisibleColumns;
+    }
+
+    public void setDefaultVisibleColumns(Set<String> defaultVisibleColumns) {
+        this.defaultVisibleColumns = defaultVisibleColumns;
+    }
+
+    public void setMetaDataColumns(Set<String> metaDataColumns) {
+        this.metaDataColumns = metaDataColumns;
+    }
+
+    public void restoreColumnOrder(TreeTableModel model) {
+        if (StringUtils.isNotEmpty(prefix) && model.getColumnCount() > 0) {
+            try {
+                String columns = userPreferences.get(prefix + "ColumnOrderNames", "");
+                if (StringUtils.isNotEmpty(columns)) {
+                    Map<String, Integer> columnOrder = (HashMap<String, Integer>) ObjectXMLSerializer.getInstance().deserialize(columns, Map.class);
+
+                    if (defaultVisibleColumns != null && !CollectionUtils.subtract(defaultVisibleColumns, columnOrder.keySet()).isEmpty()) {
+                        restoreDefaults(defaultVisibleColumns);
+                    }
+
+                    TableColumnModelExt columnModel = (TableColumnModelExt) getTableHeader().getColumnModel();
+                    if (columnModel.getColumnCount() > 0) {
+                        for (Map.Entry<String, Integer> entry : columnOrder.entrySet()) {
+                            String columnName = entry.getKey();
+                            int index = entry.getValue();
+
+                            if (index > -1) {
+                                columnModel.moveColumn(columnModel.getColumnIndex(columnName), index);
+                            }
+
+                            TableColumnExt columnExt = getColumnExt(columnName);
+                            if (columnExt != null) {
+                                getColumnExt(columnName).setVisible(index > -1);
+                            }
+                        }
+                    }
+                } else {
+                    for (int modelIndex = 0; modelIndex < columnModel.getColumnCount(); modelIndex++) {
+                        TableColumnExt column = getColumnExt(modelIndex);
+                        String columnName = column.getTitle();
+
+                        boolean defaultVisible = false;
+                        if (defaultVisibleColumns.contains(columnName)) {
+                            defaultVisible = true;
+                        }
+
+                        column.setVisible(defaultVisible);
+                    }
+                }
+            } catch (Exception e) {
+                userPreferences.put(prefix + "DefaultVisibleColumns", "");
+                userPreferences.put(prefix + "ColumnOrderNames", "");
+            }
+        }
     }
 
     public void saveSortOrder(int column) {
@@ -90,11 +164,81 @@ public class MirthTreeTable extends SortableTreeTable {
         return sortOrderColumn;
     }
 
-    public void restoreDefaults() {
+    public void restoreDefaults(Set<String> defaultColumnOrder) {
         if (StringUtils.isNotEmpty(prefix)) {
+            Map<String, Integer> columnOrder = new HashMap<String, Integer>();
+
+            setColumnSequence(defaultColumnOrder.toArray());
+
+            int index = 0;
+            for (TableColumn column : getColumns(true)) {
+                TableColumnExt columnExt = (TableColumnExt) column;
+                String columnName = columnExt.getTitle();
+
+                boolean enable = defaultColumnOrder.contains(columnExt.getTitle());
+                columnExt.setVisible(enable);
+
+                if (enable && (metaDataColumns == null || !metaDataColumns.contains(columnName))) {
+                    columnOrder.put(columnExt.getTitle(), index);
+                    index++;
+                } else {
+                    columnOrder.put(columnExt.getTitle(), -1);
+                }
+            }
+
+            savePreferences(columnOrder);
+
             resetSortOrder();
             userPreferences.put(prefix + "SortOrder", "");
             userPreferences.putInt(prefix + "SortOrderColumn", -1);
+        }
+    }
+
+    public void saveColumnOrder() {
+        if (StringUtils.isNotEmpty(prefix)) {
+            Map<String, Integer> columnOrder = new HashMap<String, Integer>();
+            Map<String, Integer> offset = new HashMap<String, Integer>();
+
+            if (metaDataColumns != null) {
+                for (TableColumn column : getColumns(true)) {
+                    String columnName = (String) column.getHeaderValue();
+                    TableColumnExt columnExt = (TableColumnExt) column;
+                    if (metaDataColumns.contains(columnName)) {
+                        offset.put(columnName, convertColumnIndexToView(columnExt.getModelIndex()));
+                    }
+                }
+            }
+
+            for (TableColumn column : getColumns(true)) {
+                String columnName = (String) column.getHeaderValue();
+                TableColumnExt columnExt = (TableColumnExt) column;
+                
+                if (columnExt.isVisible() && (metaDataColumns == null || !metaDataColumns.contains(columnName))) {
+                    int columnIndex = this.convertColumnIndexToView(columnExt.getModelIndex());
+                    if (offset.size() > 0) {
+                        for (Map.Entry<String, Integer> entry : offset.entrySet()) {
+                            int metaDataIndex = entry.getValue();
+                            if (metaDataIndex != -1 && metaDataIndex < columnIndex) {
+                                columnIndex--;
+                            }
+                        }
+                    }
+
+                    columnOrder.put(columnName, columnIndex);
+                } else {
+                    columnOrder.put(columnName, -1);
+                }
+            }
+
+            savePreferences(columnOrder);
+        }
+    }
+
+    private void savePreferences(Map<String, Integer> columnOrder) {
+        try {
+            userPreferences.put(prefix + "ColumnOrderNames", ObjectXMLSerializer.getInstance().serialize(new HashMap<String, Integer>(columnOrder)));
+        } catch (Exception e) {
+            userPreferences.put(prefix + "ColumnOrderNames", "");
         }
     }
 
@@ -119,9 +263,8 @@ public class MirthTreeTable extends SortableTreeTable {
             };
 
             /*
-             * Don't edit cells on any keystroke. Let the toggleEditing action
-             * handle it for 'Enter' only. Also surrender focus to any activated
-             * editor.
+             * Don't edit cells on any keystroke. Let the toggleEditing action handle it for 'Enter'
+             * only. Also surrender focus to any activated editor.
              */
             setAutoStartEditOnKeyStroke(false);
             setSurrendersFocusOnKeystroke(true);
@@ -183,14 +326,13 @@ public class MirthTreeTable extends SortableTreeTable {
 
     public List<AbstractDashboardTableNode> getSelectedNodes() {
         List<AbstractDashboardTableNode> nodes = new ArrayList<AbstractDashboardTableNode>();
-      
-       int[] selectedRows = this.getSelectedModelRows();
-      
-       for (int i = 0; i < selectedRows.length; i++)
-       {
-           nodes.add((AbstractDashboardTableNode) this.getPathForRow(selectedRows[i]).getLastPathComponent());
-       }
-      
-       return nodes;
-   }
+
+        int[] selectedRows = this.getSelectedModelRows();
+
+        for (int i = 0; i < selectedRows.length; i++) {
+            nodes.add((AbstractDashboardTableNode) this.getPathForRow(selectedRows[i]).getLastPathComponent());
+        }
+
+        return nodes;
+    }
 }

rogin/engine

commit 8e514333fd1a59e05776b99f5f9515c7eb89e8fd

Use type-appropriate renderer for custom metadata columns in message browser
This fixes what was apparently a regression bug in Mirth 3.2.0
introduced by commit 70eba57551192a8ee62d604a959f06fb81f38fc6. It
restores the functionality to display the contents of custom metadata
columns in a format appropriate to the column type rather than always
using the default MessageBrowserTextCellRenderer for non-String types.

Signed-off-by: Tony Germano <tony@germano.name>
Commit-message-by: Tony Germano <tony@germano.name>
Issue: https://github.com/OpenIntegrationEngine/engine/issues/75
Related: https://github.com/nextgenhealthcare/connect/issues/6203

client/src/com/mirth/connect/client/ui/browsers/message/MessageBrowser.java
@@ -382,6 +382,9 @@ public class MessageBrowser extends javax.swing.JPanel {
         MessageBrowserTableColumnFactory columnFactory = (MessageBrowserTableColumnFactory) messageTreeTable.getColumnFactory();
         for (int modelIndex = 0; modelIndex < columnList.size(); modelIndex++) {
             TableColumnExt column = columnFactory.createAndConfigureTableColumn(messageTreeTable.getModel(), modelIndex);
+            if (modelIndex >= columnMap.size()) {
+                columnFactory.configureCustomColumn(column, metaDataColumns.get(modelIndex - columnMap.size()).getType());
+            }
             messageTreeTable.addColumn(column);
         }
 